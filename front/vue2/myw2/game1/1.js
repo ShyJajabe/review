4c0fa2da1f389bdd59f8f7c6d09baeb188e8a64f
ShyJajabe <shinyjajabe@outlook.com>
2017-07-03 17:32:18 +0800

    7/3

--- front/vue2/myw2/game1/1.js
+++ front/vue2/myw2/game1/1.js
@@ -7,18 +7,52 @@ new Vue({
         playerHealth:100,
         monsterHealth:100,
         showButton:true,
-
+        turns:[]
     },
     methods:{
-        playerAttack:function(){
-            this.monsterHealth=this.monsterHealth-10;
+        attack:function(){
+            var damage=this.calculateDamage(3,10);
+            this.monsterHealth=this.monsterHealth-damage;
+            this.turns.unshift({
+                styleClass:'pAttack',
+                text:'player hits monster for'+damage,
+            });
+            if(this.checkWin()){
+                return false;
+            }
+            this.monsterAttack();
+        },
+        specialAttack:function(){
+            var damage=this.calculateDamage(10,20);
+            this.monsterHealth=this.monsterHealth-damage;
+            this.turns.unshift({
+                styleClass:'sAttack',
+                text:'player hits monster specially for'+damage,
+            });
+            if(this.checkWin()){
+                return false;
+            }
             this.monsterAttack();
         },
         monsterAttack:function(){
-            this.playerHealth=this.playerHealth-5;
+            var damage=this.calculateDamage(5,12);
+            this.playerHealth=this.playerHealth-damage;
+            this.turns.unshift({
+                styleClass:'mAttack',
+                text:'monster hits player for'+damage,
+            });
+            this.checkWin();
         },
         playerHeal:function(){
-            this.playerHealth= this.playerHealth+3;
+            if(this.playerHealth<=90){
+            this.playerHealth= this.playerHealth+10;}
+            else{
+                this.playerHealth=100;
+            }
+            this.turns.unshift({
+                styleClass:'pHeal',
+                text:'player heal for 10',
+            });
             this.monsterAttack();
         },
         giveUp:function(){
@@ -28,6 +62,31 @@ new Vue({
             this.playerHealth=100;
             this.monsterHealth=100;
             this.showButton=false;
+            this.turns = [];
+        },
+        calculateDamage:function(min,max){
+            return Math.max(Math.floor(Math.random()*max)+1,min);
+        },
+        checkWin:function(){
+            if(this.monsterHealth<=0){
+                if(confirm('you won')){
+                    this.newGame();
+                }
+                else{
+                    this.showButton=true;
+                }
+                return true
+            }
+            else if(this.playerHealth<=0){
+                if(confirm('you lose')){
+                    this.newGame();
+                }
+                else{
+                    this.showButton=true;
+                }
+                return true
+            }
+            return false;
         }
 
     },
