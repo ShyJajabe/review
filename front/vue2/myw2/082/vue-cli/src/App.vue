24d0b1a568cbb48f3df311d2578522b8b37cdf0c
ShyJajabe <shinyjajabe@outlook.com>
2017-07-06 20:28:07 +0800

    7/6

---
+++ front/vue2/myw2/082/vue-cli/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div><test v-for="a in 5"></test></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/vue2/myw2/082/vue-cli/src/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>{{status}}<button @click="change">click</button></div>
+
+</template>
+<script>
+    export default{
+        data:function () {
+            return {
+                status:'ssss'
+            }
+        },
+        methods:{
+            change:function(){
+                this.status='aaa'
+            }
+        }
+    }
+</script>
+<style></style>
