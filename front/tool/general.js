e0c7a59c39cd58acc0ec104db504daad16c765b5
ShyJajabe <shinyjajabe@outlook.com>
2017-07-20 09:47:09 +0800

    7/20

--- front/tool/general.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by admin on 2017/4/18.
- */
-String.prototype.replaceTag= function(reallyDo, replaceWith, ignoreCase) {
-    if (!RegExp.prototype.isPrototypeOf(reallyDo)) {
-        return this.replace(new RegExp(reallyDo, (ignoreCase ? "gi": "g")), replaceWith);
-    } else {
-        return this.replace(reallyDo, replaceWith);
-    }};
